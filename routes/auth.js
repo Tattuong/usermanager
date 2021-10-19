201c1c0594ca20596f10f839994b896fd454710a
fun <you@ex>
2021-10-19 17:24:34 +0700

    update

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,11 @@ const express = require('express')
 const User = require("../models/user")
 const router = express.Router()
 
-router.get('/',(req, res) =>{
-    res.render('/index')
+
+
+
+router.get('/' ,(req, res) =>{
+         res.render("index")
 })
 
 
--- server.js
+++ server.js
@@ -1,36 +1,35 @@
-const express = require('express');
-const { router } = require('json-server');
+const express = require("express");
 const mongoose = require("mongoose");
-const path = require('path')
-const app = express()
+const path = require("path");
+const app = express();
+const bodyParser = require("body-parser");
 
-const apiRouter =  require("./routes/auth")
-const connectDb = async () =>{
+const apiRouter = require("./routes/auth");
+
+const connectDb = async () => {
   try {
-    await mongoose.connect(
-      'mongodb://localhost:27017/usermanagement',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    )
+    await mongoose.connect("mongodb://localhost:27017/usermanagement", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected");
-    
   } catch (error) {
     console.log(error.message);
-    process.exit(1)
+    process.exit(1);
   }
-}
+};
 connectDb();
 
-
-app.use(express.json())
-app.use('/', apiRouter)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+
 //set view engine
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", apiRouter);
 
 const PORT = 3002;
-app.listen(PORT, () => console.log(`server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
